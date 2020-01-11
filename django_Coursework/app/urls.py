7bc6822d3065970da9f568fa49b331a359ca6354
blacrzrae123 <58897191+blacrzrae123@users.noreply.github.com>
2020-01-11 22:58:56 +0545

    adding search

--- django_Coursework/app/urls.py
+++ django_Coursework/app/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('hello/',view_hello_world_test),
     path('test/',view_student_page),
     path('list/',view_student_lists),
     path('studentform/',view_student_form),
@@ -11,4 +10,5 @@ urlpatterns = [
     path('list/edit/<int:ID>',view_Studentdata_updateform),
     path('list/edit/update/<int:ID>',view_update_form_data_in_db),
     path('list/delete/<int:ID>',deletestudent),
+    path('search/',search),
 ]

cd60be2c983b7b04fafc6f9566e481a2196e4a3b
SafalGhimire <58987042+SafalGhimire@users.noreply.github.com>
2020-01-24 23:13:07 +0545

    Adding login_logout feature

--- django_Coursework/django_Coursework/urls.py
+++ django_Coursework/django_Coursework/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import *
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from app.views import view_authenticate_user
+from app.views import view_logout
 
 from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
+
 
 ]
 
@@ -17,4 +18,18 @@ urlpatterns += [
     path('app/',include('app.urls'))
 
 ]
+urlpatterns += [
+    # path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/login/',view_authenticate_user)
+    # path('accounts/',include('django.contrib.auth.urls')),
 
+]
+urlpatterns += [
+    # path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/logout/',view_logout)
+    # path('accounts/',include('django.contrib.auth.urls')),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
